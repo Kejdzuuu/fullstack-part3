35fba628f658eb809f15852ee88f78e08eb22ee6
Muciej <kejdzuuu@gmail.com>
2020-09-13 13:26:20 +0200

    Ex. 3.1

---
+++ phonebook/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+  {
+    name: "Kimi Raikkonen",
+    number: "0123-567",
+    id: 1
+  },
+  {
+    name: "Valtteri Bottas",
+    number: "2",
+    id: 2
+  },
+  {
+    name: "Dennis Ritchie",
+    number: "441242-22",
+    id: 3
+  }
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
