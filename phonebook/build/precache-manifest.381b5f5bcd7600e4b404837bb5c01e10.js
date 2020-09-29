2e07debc43851be35690b8e50eff67bf96e728e9
Muciej <kejdzuuu@gmail.com>
2020-09-30 01:06:29 +0200

    Ex. 3.11

---
+++ phonebook/build/precache-manifest.381b5f5bcd7600e4b404837bb5c01e10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6659599e0426e787301970914b4b0c93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a30622c4f74bed9005e",
+    "url": "/static/css/main.9169c767.chunk.css"
+  },
+  {
+    "revision": "f24c868740ab0f7c25f5",
+    "url": "/static/js/2.4b8e90bc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4b8e90bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a30622c4f74bed9005e",
+    "url": "/static/js/main.829fc364.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
--- phonebook/index.js
+++ phonebook/index.js
@@ -10,6 +10,7 @@ morgan.token('data', function (req, res) {
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static('build'))
 app.use(morgan(':method :url :status :response-time :data'))
 
 let persons = [
